2ecc01159797d4342b6ee723683a261d73cbbe83
MADISON VELEZ <maddievbusiness1143@gmail.com>
2019-06-05 13:50:46 -0400

    When zombies move into you, they stop. Also fixed code errors and formatting. -MAV

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,7 @@
 public class Game {
 
-  // "APOCALYPSE OF THE DAMNED" created by MADISON VELEZ, AHMED DIABY, AND JARED CID
+  // "APOCALYPSE OF THE DAMNED" created by MADISON VELEZ, AHMED DIABY, AND JARED
+  // CID
 
   private Grid grid;
   private int userRow;
@@ -13,15 +14,15 @@ public class Game {
   private WavPlayer backgroundMusic;
   private int health;
   private String player = "images\\jeremyHeere.gif";
-  private String zPic ="images\\zombie2.gif";
-  private String[] zombies = {"images\\zombie1.gif", "images\\zombie2.gif", "images\\zombie3.gif"};
+  private String zPic = "images\\zombie2.gif";
+  private String[] zombies = { "images\\zombie1.gif", "images\\zombie2.gif", "images\\zombie3.gif" };
   private int time;
-  
+
   // MAIN CLASS
-  
+
   public Game() {
     grid = new Grid(10, 20);
-    userRow = grid.getNumRows()/2;  // USER IS ALWAYS IN THE MIDDLE OF THE GRID WHEN PARAMETERS CHANGE
+    userRow = grid.getNumRows() / 2; // USER IS ALWAYS IN THE MIDDLE OF THE GRID WHEN PARAMETERS CHANGE
     userCol = 0;
     rowLength = grid.getNumRows();
     colLength = grid.getNumCols();
@@ -36,7 +37,7 @@ public class Game {
     grid.setMovableBackground("images\\mainBackground.png", 0, 0, 1.0, 1.0);
     grid.setImage(new Location(userRow, userCol), player);
   }
-  
+
   public void play() {
     while (!isGameOver()) {
       grid.pause(100);
@@ -49,87 +50,87 @@ public class Game {
       msElapsed += 100;
     }
   }
-  
+
   public void handleKeyPress() {
     int key = grid.checkLastKeyPressed();
     System.out.println(key);
-    if(key == 38 || key == 87) {  // UP
-     if(userRow <= 3) {
-       return;
-     }
-     userRow--;
-     Location loc = new Location(userRow, userCol);
-     grid.setImage(loc, player);
-     Location oldLoc = new Location(userRow + 1, userCol);
-     grid.setImage(oldLoc, null);
-    } else if(key == 40 || key == 83) { // DOWN
-      if(userRow >= 9) {
+    if (key == 38 || key == 87) { // UP
+      if (userRow <= 3) {
         return;
       }
-     userRow++;
-     Location loc = new Location(userRow, userCol);
-     grid.setImage(loc, player);
-     Location oldLoc = new Location(userRow - 1, userCol);
-     grid.setImage(oldLoc, null);
-    } else if(key == 37 || key == 65) { // LEFT
-     if(userCol <= 0) {
-       return;
+      userRow--;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, player);
+      Location oldLoc = new Location(userRow + 1, userCol);
+      grid.setImage(oldLoc, null);
+    } else if (key == 40 || key == 83) { // DOWN
+      if (userRow >= 9) {
+        return;
       }
-     userCol--;
-     Location loc = new Location(userRow, userCol);
-     grid.setImage(loc, player);
-     Location oldLoc = new Location(userRow, userCol + 1);
-     grid.setImage(oldLoc, null);
-    } else if(key == 39 || key == 68) { // RIGHT
-     if(userCol >= 19) {
-       return;
-     }
-     userCol++;
-     Location loc = new Location(userRow, userCol);
-     grid.setImage(loc, player);
-     Location oldLoc = new Location(userRow, userCol - 1);
-     grid.setImage(oldLoc, null);
+      userRow++;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, player);
+      Location oldLoc = new Location(userRow - 1, userCol);
+      grid.setImage(oldLoc, null);
+    } else if (key == 37 || key == 65) { // LEFT
+      if (userCol <= 0) {
+        return;
+      }
+      userCol--;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, player);
+      Location oldLoc = new Location(userRow, userCol + 1);
+      grid.setImage(oldLoc, null);
+    } else if (key == 39 || key == 68) { // RIGHT
+      if (userCol >= 19) {
+        return;
+      }
+      userCol++;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, player);
+      Location oldLoc = new Location(userRow, userCol - 1);
+      grid.setImage(oldLoc, null);
     }
- }
+  }
 
   public void battleIntegration() {
 
   }
 
   public void populateRightEdge() {
-      int place = 3 + (int)(Math.random() * (grid.getNumRows() - 3));
+    if (Math.random() < 0.50) {
+      int place = 3 + (int) (Math.random() * (grid.getNumRows() - 3));
       Location loc = new Location(place, grid.getNumCols() - 1);
-
-
       zPic = zombies[(int) (Math.random() * zombies.length)];
-
       grid.setImage(loc, zPic);
-  }
-  
-  public void scrollLeft() {
-    for(int i = 0; i < grid.getNumRows(); i++){
-       for(int j = 0; j < grid.getNumCols(); j++){
-         Location loc = new Location(i, j);
-         Location end = new Location(i, 0);
-         if(zPic.equals(grid.getImage(end))){
-           grid.setImage(end, null);
-         }
-         if(zPic.equals(grid.getImage(loc))){
-           Location enemyLoc = new Location(i, j -1);
-           grid.setImage(enemyLoc, zPic);
-           Location oldLoc = new Location(i,j);
-           grid.setImage(oldLoc, null);
-           handleCollision(enemyLoc);
-        }
-     }
     }
   }
-  
+
+  public void scrollLeft() {
+    for (int i = 0; i < grid.getNumRows(); i++) {
+      for (int j = 0; j < grid.getNumCols(); j++) {
+        Location oldLoc = new Location(i, j);
+        Location end = new Location(i, 0);
+        Location newLoc = new Location(i, j - 1);
+        for (int z = 0; z < zombies.length; z++) {
+          if (j == 0) {
+            grid.setImage(end, null);
+          } else if (zombies[z].equals(grid.getImage(oldLoc)) && player.equals(grid.getImage(newLoc))) {
+            battleIntegration();
+          } else if (zombies[z].equals(grid.getImage(oldLoc))) {
+            grid.setImage(oldLoc, null);
+            grid.setImage(newLoc, zombies[z]);
+            handleCollision(newLoc);
+          }
+          grid.setImage(new Location(userRow, userCol), player);
+        }
+
+      }
+    }
+  }
+
   public void handleCollision(Location zLoc) {
-    if(zLoc.equals(new Location(userRow + 1, userCol))||
-    zLoc.equals(new Location(userRow - 1, userCol))||
-    zLoc.equals(new Location(userRow, userCol + 1))||
-    zLoc.equals(new Location(userRow, userCol - 1))) {
+    if (zLoc.equals(new Location(userRow, userCol + 1))) {
       health--;
     }
   }
@@ -137,7 +138,7 @@ public class Game {
   public int getTime() {
     return time;
   }
-  
+
   public int getHealth() {
     return health;
   }
@@ -145,24 +146,24 @@ public class Game {
   public int getScore() {
     return 0;
   }
-  
+
   public void updateTitle() {
     grid.setTitle("Game:  " + getScore() + " Health:  " + getHealth() + " Time:  " + getTime());
   }
-  
+
   public boolean isGameOver() {
     return false;
   }
-  public void setUserRow(){
+
+  public void setUserRow() {
     int rowLength = grid.getNumRows();
-    if(userRow > rowLength){
+    if (userRow > rowLength) {
       userRow = rowLength - 1;
-    }
-    else if(userRow < 0){
+    } else if (userRow < 0) {
       userRow = 0;
     }
   }
-    
+
   public static void main(String[] args) {
     Game game = new Game();
     game.play();
